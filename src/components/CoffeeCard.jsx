03539c4999a80c5a19f6d5e2dd2afc93b552898c
farhanshahriyar <farhan.abir9999@gmail.com>
2023-10-18 20:56:33 +0600

    added delete client functions

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,13 +1,38 @@
-
+import Swal from 'sweetalert2'
 
 const CoffeeCard = ({coffee}) => {
-    const { name,
+    const { _id,
+         name,
         quantity,
         supplier,
         taste,
         category,
         details,
         photo} = coffee
+    
+        const handleDelete = () => {
+            // console.log(_id)
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+                  console.log("file has been deleted.")
+                }
+              })
+        }
+
+
   return (
 
     <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
@@ -34,13 +59,27 @@ const CoffeeCard = ({coffee}) => {
         <p className="font-bold">Quantity : </p> {quantity}
       </p>
     </div>
-    <div className="p-6 pt-0">
+    <div className="p-6 pt-2 flex gap-2">
       <button
-        className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
         data-ripple-light="true"
       >
-        Read More
+        Edit
+      </button>
+      <button onClick={() => (handleDelete(_id))}
+        className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        type="button"
+        data-ripple-light="true"
+      >
+        Delete
+      </button>
+      <button
+        className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        type="button"
+        data-ripple-light="true"
+      >
+        View
       </button>
     </div>
   </div>
